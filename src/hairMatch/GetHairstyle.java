815382e4f9c65fae4f72b2730efa93956c1e90f3
huang <linda0981347815@gmail.com>
2020-05-09 15:26:41 +0800

    style: modify GetHairstyle return value

--- src/hairMatch/GetHairstyle.java
+++ src/hairMatch/GetHairstyle.java
@@ -4,25 +4,25 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 public class GetHairstyle {
 
 	public String getAllPictures(String url, String hairstyleFolderRealPath, String type) throws IOException {
 		String path = hairstyleFolderRealPath + "/" + type;
-		ArrayList<String> allHairstyle = new ArrayList<String>();
+		ArrayList<String> photos = new ArrayList<String>();
 		try {
 			File user = new File(path);
 			String[] filenames;
 			filenames = user.list();
 			for (int i = 0; i < filenames.length; i++) {
-				allHairstyle.add(url + "/" + filenames[i]);
+				photos.add(filenames[i]);
 			}
 		} catch (Exception e) {
 			System.out.println("此資料夾不存在");
 		}
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String jsonStr = gson.toJson(allHairstyle); // 轉成JSON檔
+		HairStyle hairStyle = new HairStyle(url, photos);
+		Gson gson = new Gson();
+		String jsonStr = gson.toJson(hairStyle);
 		return jsonStr;
 	}
 }

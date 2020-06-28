f1bfbaec7b4c333995351abaabf0113e89a0afd3
huang <linda0981347815@gmail.com>
2020-06-28 21:46:00 +0800

    fix: modify favorite null to empty array

--- src/jdbc/FavoriteMySQL.java
+++ src/jdbc/FavoriteMySQL.java
@@ -21,18 +21,20 @@ public class FavoriteMySQL {
 			rs = stat.executeQuery("select salon from favorite where account =" + "\"" + account + "\"");
 			if (rs.next()) {
 				salon = rs.getString("salon");
-				if(salon.equals(" ")) {
-					salon="[]";
+				if (salon.equals(" ")) {
+					salon = "[]";
 				}
+			} else {
+				salon = "[]";
 			}
 		} catch (SQLException e) {
 			System.out.println("select table SQLException:" + e.toString());
 		} finally {
 			database.close();
 		}
-		return "{\"id\":"+salon+"}";
+		return "{\"id\":" + salon + "}";
 	}
-	
+
 	public String getStylistId(String account) {
 		String stylist = null;
 		try {
@@ -40,18 +42,20 @@ public class FavoriteMySQL {
 			rs = stat.executeQuery("select stylist from favorite where account =" + "\"" + account + "\"");
 			if (rs.next()) {
 				stylist = rs.getString("stylist");
-				if(stylist.equals(" ")) {
-					stylist="[]";
+				if (stylist.equals(" ")) {
+					stylist = "[]";
 				}
+			} else {
+				stylist = "[]";
 			}
 		} catch (SQLException e) {
 			System.out.println("select table SQLException:" + e.toString());
 		} finally {
 			database.close();
 		}
-		return "{\"id\":"+stylist+"}";
+		return "{\"id\":" + stylist + "}";
 	}
-	
+
 	public String getStylistWorksId(String account) {
 		String stylist_works = null;
 		try {
@@ -59,18 +63,20 @@ public class FavoriteMySQL {
 			rs = stat.executeQuery("select stylist_works from favorite where account =" + "\"" + account + "\"");
 			if (rs.next()) {
 				stylist_works = rs.getString("stylist_works");
-				if(stylist_works.equals(" ")) {
-					stylist_works="[]";
+				if (stylist_works.equals(" ")) {
+					stylist_works = "[]";
 				}
+			} else {
+				stylist_works = "[]";
 			}
 		} catch (SQLException e) {
 			System.out.println("select table SQLException:" + e.toString());
 		} finally {
 			database.close();
 		}
-		return "{\"id\":"+stylist_works+"}";
+		return "{\"id\":" + stylist_works + "}";
 	}
-	
+
 	public boolean addSalon(String account, int num) {
 		int salonID;
 		int flag = 0; // 正確的輸出1 錯誤輸出0
@@ -88,6 +94,8 @@ public class FavoriteMySQL {
 					String salon = RS.getString("salon");
 					if (salon.equals(" ")) // 沒有加過店家在我的最愛
 						salon = "[" + String.valueOf(salonID) + "]";
+					else if (salon.equals("[]"))
+						salon = salon.substring(0, salon.length() - 1) + String.valueOf(salonID) + "]";
 					else
 						salon = salon.substring(0, salon.length() - 1) + "," + String.valueOf(salonID) + "]";
 					String update = "update favorite set salon=? where account=?";
@@ -137,6 +145,8 @@ public class FavoriteMySQL {
 					String stylist = RS.getString("stylist");
 					if (stylist.equals(" ")) // 沒有加過設計師在我的最愛
 						stylist = "[" + String.valueOf(stylistID) + "]";
+					else if (stylist.equals("[]"))
+						stylist = stylist.substring(0, stylist.length() - 1) + String.valueOf(stylistID) + "]";
 					else
 						stylist = stylist.substring(0, stylist.length() - 1) + "," + String.valueOf(stylistID) + "]";
 					String update = "update favorite set stylist=? where account=?";
@@ -186,8 +196,12 @@ public class FavoriteMySQL {
 					String stylist_works = RS.getString("stylist_works");
 					if (stylist_works.equals(" ")) // 沒有加過設計師作品在我的最愛
 						stylist_works = "[" + String.valueOf(stylist_works_ID) + "]";
+					else if (stylist_works.equals("[]"))
+						stylist_works = stylist_works.substring(0, stylist_works.length() - 1)
+								+ String.valueOf(stylist_works_ID) + "]";
 					else
-						stylist_works = stylist_works.substring(0, stylist_works.length() - 1) + "," + String.valueOf(stylist_works_ID) + "]";
+						stylist_works = stylist_works.substring(0, stylist_works.length() - 1) + ","
+								+ String.valueOf(stylist_works_ID) + "]";
 					String update = "update favorite set stylist_works=? where account=?";
 					PreparedStatement pst = (PreparedStatement) con.prepareStatement(update);
 					pst.setString(1, stylist_works); // 傳送第1個參數(取代第一個問號)
@@ -344,28 +358,28 @@ public class FavoriteMySQL {
 
 	public static void main(String args[]) {
 		FavoriteMySQL test = new FavoriteMySQL();
-		String account = "oh";
+		String account = "test";
 
-//		boolean salon = test.addSalon(account, 3);
+		boolean salon = test.addSalon(account, 1);
+		System.out.println("salon boolean -> " + salon);
+		boolean stylist = test.addStylist(account, 14);
+		System.out.println("stylist boolean -> " + stylist);
+		boolean stylistWorks = test.addStylistWorks(account, 21);
+		System.out.println("stylistWorks boolean -> " + stylistWorks);
+
+//		boolean salon = test.deleteSalon(account, 1);
 //		System.out.println("salon boolean -> " + salon);
-//		boolean stylist = test.addStylist(account, 14);
+//		boolean stylist = test.deleteStylist(account, 11);
 //		System.out.println("stylist boolean -> " + stylist);
-//		boolean stylistWorks = test.addStylistWorks(account, 24);
+//		boolean stylistWorks = test.deleteStylistWorks(account, 23);
 //		System.out.println("stylistWorks boolean -> " + stylistWorks);
 
-//		boolean salon = test.deleteSalon(account, 3);
-//		System.out.println("salon boolean -> " + salon);
-//		boolean stylist = test.deleteStylist(account, 14);
-//		System.out.println("stylist boolean -> " + stylist);
-//		boolean stylistWorks = test.deleteStylistWorks(account, 22);
-//		System.out.println("stylistWorks boolean -> " + stylistWorks);
-		
-		String salon = test.getSalonId(account);
-		System.out.println("salon -> " + salon);
-		String stylist = test.getStylistId(account);
-		System.out.println("stylist -> " + stylist);
-		String stylistWorks = test.getStylistWorksId(account);
-		System.out.println("stylistWorks -> " + stylistWorks);
+//		String salon = test.getSalonId(account);
+//		System.out.println("salon -> " + salon);
+//		String stylist = test.getStylistId(account);
+//		System.out.println("stylist -> " + stylist);
+//		String stylistWorks = test.getStylistWorksId(account);
+//		System.out.println("stylistWorks -> " + stylistWorks);
 	}
 
 }

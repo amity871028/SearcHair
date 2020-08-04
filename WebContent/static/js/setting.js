09cea903db4d026178342da3fd09b8d4f519e1f6
amity <amity3744039@gmail.com>
2020-08-04 19:44:19 +0800

    feat: finish hair analysis frontend

--- WebContent/static/js/setting.js
+++ WebContent/static/js/setting.js
@@ -1,9 +1,17 @@
 const settingAPI = {
 	password: 'api-user-password-update',
+	hairAnaysis: 'api-setting',
 	remind: 'api-user-remind'
 };
 const PASSWORD = ['old-password', 'new-password', 'confirm-new-password'];
 const account = localStorage.getItem('account');
+const hairAnalysis = {
+		oily: "油性髮質",
+		dry: "乾性髮質",
+		normal: "中性髮質",
+		combination: "混和性髮質",
+		damaged: "受損髮質"
+}
 
 /* ----check the fields of the password---- */
 function validatePassword() {
@@ -74,6 +82,32 @@ async function updateRemindFrequency(){
 	}
 }
 
+async function updateHairAnalysis(){
+	if (document.forms['hair-analysis-form'].reportValidity()) {
+		const result = await FetchData.post(settingAPI.hairAnaysis, {
+			account: account,
+			greasy: $('input:radio[name="greasy"]:checked')[0].value === "true",
+			frizzy: $('input:radio[name="frizzy"]:checked')[0].value === "true",
+			sleek: $('input:radio[name="sleek"]:checked')[0].value === "true",
+			tangled: $('input:radio[name="tangled"]:checked')[0].value === "true",
+			perm: $('input:radio[name="perm"]:checked')[0].value === "true"
+		});
+		const json = await result.json();
+		if(result.status == 200){
+			localStorage.setItem('hair-analysis', json.hairAnalysis);
+			updateHairResult();
+		}
+	}
+}
+
+function updateHairResult(){
+
+	document.getElementById('result').setAttribute('style', 'display: initial;');
+	document.getElementById('hair-analysis-form').setAttribute('style', 'display: none;');
+	
+	let hair = hairAnalysis[localStorage.getItem('hair-analysis')];
+	document.getElementById('hair-result').innerHTML = hair;
+}
 
 function sidebarSetting(){
     document.getElementById('dismiss').addEventListener('click', function(){
@@ -106,6 +140,15 @@ function init(){
     	else frequency.disabled = true;
     });
     document.getElementById('update-notice-btn').addEventListener('click', updateRemindFrequency);
+    
+    
+    if(localStorage.getItem('hair-analysis')) updateHairResult();
+    document.getElementById('hair-analysis-btn').addEventListener('click', updateHairAnalysis);
+    document.getElementById('update-form-btn').addEventListener('click', function(){
+    	document.getElementById('result').setAttribute('style', 'display: none;');
+    	document.getElementById('hair-analysis-form').setAttribute('style', 'display: initial;');
+    });
+    
 }
 
 window.addEventListener('load', init);
